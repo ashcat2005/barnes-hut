e9aec357f465c90fabc23cfb430715906f28ac00
Juan David Ochoa Acosta <beyonder88@MacBook-Air-de-Juan.local>
2021-10-05 19:15:49 -0500

    added tree scaling

--- main/common.py
+++ main/common.py
@@ -1,7 +1,7 @@
 # Barnes-Hut Algorithm
 
 from copy import deepcopy
-from numpy import array, ones, empty, random, sqrt, exp, pi, sin, cos, arctan
+from numpy import array, ones, empty, random, sqrt, exp, pi, sin, cos, arctan, zeros
 from numpy.linalg import norm
 import scipy.integrate as integrate
 from scipy.optimize import fsolve
@@ -21,6 +21,9 @@ dt = 1.e-3 # Gyr
 # Theta-criterion of the Barnes-Hut algorithm.
 theta = 0.3
 
+#Radius on the image
+scale_factor=.025 #---> 0.25/ini_radius
+
 #####################################################################################
 
 class Node:
@@ -39,6 +42,7 @@ class Node:
         self.m_pos = m * position
         self.momentum = momentum
         self.child = None
+        self.force = None
     
     def position(self):
         '''
@@ -116,7 +120,11 @@ def distance_between(node1, node2):
     '''
     Returns the distance between node1 and node2.
     '''
-    return norm(node1.position() - node2.position())
+    #print(node1.position())
+    #print(node2.position())
+    #print(norm(node1.position() - node2.position()))
+    #print("")
+    return norm(node1.position() - node2.position())/scale_factor
 
 def gravitational_force(node1, node2):
     '''
@@ -128,12 +136,13 @@ def gravitational_force(node1, node2):
     d = distance_between(node1, node2)
     if d < cutoff_dist:
         #print('Collision!')
-        # Returns no Force!
+        #Returns no Force!
         return array([0., 0., 0.])
+        
     else:
         # Gravitational force
-        return G*node1.m*node2.m*(node1.position() - node2.position())/d**3
-
+        return G*node1.m*node2.m*(node1.position() - node2.position())/d**3/scale_factor
+    
 def force_on(body, node, theta):
     '''
     # Barnes-Hut algorithm: usage of the quad-tree. This function computes
@@ -148,9 +157,9 @@ def force_on(body, node, theta):
 
     # 2. Otherwise, calculate the ratio s/d. If s/d < θ, treat this internal
     #    node as a single body, and calculate the force it exerts on body b.
-    if node.size < distance_between(node,body)*theta:#node.distance(body) * theta:
+    if node.size < scale_factor*distance_between(node,body)*theta:#node.distance(body) * theta:
         return gravitational_force(node,body)#node.force_on(body)
-
+        
     # 3. Otherwise, run the procedure recursively on each child.
     return sum(force_on(body, c, theta) for c in node.child if c is not None)
 
@@ -159,29 +168,11 @@ def verlet(bodies, root, theta, dt):
     Verlet method for time evolution.
     '''
     for body in bodies:
-        force = force_on(body, root, theta)
-        body.momentum += force*dt
-        body.m_pos += body.momentum*dt
-        
-def PEFRL(bodies, root, theta, dt):
-    '''
-    PEFRL method for time evolution.
-    '''
-    epsilon=0.1786178958448091
-    lannnda=-0.2123418310626054
-    xsi=-0.06626458266981849
-    for body in bodies:
-        body.m_pos += epsilon*body.momentum*dt
-        body.momentum += .5*(1-2*lannnda)*dt*force_on(body, root, theta)
-        body.m_pos += xsi*body.momentum*dt
-        body.momentum += lannnda*dt*force_on(body, root, theta)
-        body.m_pos += (1-2*(xsi*epsilon))*body.momentum*dt
-        body.momentum += lannnda*dt*force_on(body, root, theta)
-        body.m_pos += xsi*body.momentum*dt
-        body.momentum += .5*(1-2*lannnda)*dt*force_on(body, root, theta)
-        body.m_pos += epsilon*body.momentum*dt
-
-def random_generate(N, max_mass, BHM, center, ini_radius):
+        body.force = force_on(body, root, theta)
+        body.momentum += body.force*dt
+        body.m_pos += scale_factor*body.momentum*dt
+                  
+def uniform_galaxy(N, max_mass, BHM, center, ini_radius):
     '''
     Randomly generate the system of N particles.
     Returns
@@ -192,7 +183,7 @@ def random_generate(N, max_mass, BHM, center, ini_radius):
     # We will generate K=2*N random particles from which we will chose
     # only N-1 bodies for the system
     K = 2*N
-    random.seed(413)
+    random.seed(2)
     masses = empty([N-1])
     positions = empty([N-1,3])
     momenta = empty([N-1,3])
@@ -200,22 +191,21 @@ def random_generate(N, max_mass, BHM, center, ini_radius):
     mass = random.random(K)*(max_mass-1.) + 1.
     # x-, y- and z- positions are initialized inside a square with
     # a side of length = 2*ini_radius.
-    posx = random.random(K) *2.*ini_radius + center[0]-ini_radius
-    posy = random.random(K) *2.*ini_radius + center[1]-ini_radius
-    posz = random.random(K) *2.*ini_radius*0 + center[2]-ini_radius*0
+    posx = random.random(K) *2.*ini_radius-ini_radius
+    posy = random.random(K) *2.*ini_radius-ini_radius
+    posz = zeros(K)
     i=0
     j=0
     #Loop until complete the random N-1 bodies or use the K generated bodies
     while i<K and j<N-1:
         position = array([posx[i],posy[i],posz[i]])
-        r = position - center
-        norm_r = norm(r)
+        norm_r = norm(position)
         if norm_r < ini_radius:
             masses[j] = mass[i]
-            positions[j] = position
+            positions[j] = scale_factor*position+center
             # We use the projection of the Keplerina velocity to define the momentum
             Kep_v = sqrt(G*BHM/norm_r) # Keplerian velocity
-            momenta[j] = mass[i]*Kep_v*array([-r[1], r[0], 0.])/norm_r
+            momenta[j] = mass[i]*Kep_v*array([-position[1], position[0], 0.])/norm_r
             j+=1
         i+=1
     return masses, positions, momenta
@@ -295,33 +285,30 @@ def spiral_galaxy(N, max_mass, BHM, center, ini_radius, alpha, beta):
         momenta[i] = masses[i]*Kep_v*vec_mom
     return masses, positions, momenta
 
-def second_galaxy(bodies):
+def second_galaxy(bodies,ini_radius):
     """
     Creation of a secundary galaxy to simulate the merge of two galaxies
-    --Still in progress--
     """
     #BlackHole in the center of the secondary galaxy
-    BHM_2 = 100000                          # Solar masses
-    center_2 = array([30.0,30.0,0])         # Location of the SBH
-    BHmomentum_2 = array([0,0.,0.])  # Momentum of the SBH
+    BHM_2 = 1.e5                            # Solar masses
+    center_2 = array([.8,.8,0])         # Location of the SBH
+    BHmomentum_2 = array([-BHM_2*.1,0,0.])  # Momentum of the SBH
     #Number of bodies
-    N_2=100
+    N_2=30
     #Mass of the N bodies
     max_mass_2= 50.                     # Solar masses
     # Initial radius of the distribution
-    ini_radius_2 = 9. #kpc
-    #Parameters of the galaxy plane orientation 
-    beta_2=pi/5            #Inclination
-    alpha_2=0           #Angle in the plain x,y
-    masses_2, positions_2, momenta_2 = spiral_galaxy(N_2, max_mass_2, BHM_2, center_2, 
-                                                     ini_radius_2,alpha_2,beta_2)
-    momenta_2=momenta_2+BHmomentum_2
+    ini_radius_2 = .5*ini_radius        #kpc
+    masses_2, positions_2, momenta_2 = uniform_galaxy(N_2, max_mass_2, BHM_2, center_2, 
+                                                     ini_radius_2)
     bodies.append(Node(BHM_2, position=center_2, momentum=BHmomentum_2))
-    for i in range(N_2):
-       bodies.append(Node(masses_2[i], positions_2[i], momenta_2[i]))
-    return bodies
   
-def system_init(N, max_mass, BHM, center, BHmomentum, ini_radius):
+    for i in range(len(masses_2)-1):
+        momenta_2[i]=momenta_2[i]+BHmomentum_2/BHM_2*masses_2[i]
+        bodies.append(Node(masses_2[i], positions_2[i], momenta_2[i]))
+    return bodies
+
+def system_init(N, max_mass, BHM, center, BHmomentum, ini_radius, alpha, beta):
     '''
     This function initialize the N-body system by randomly defining
     the position vectors fo the bodies and creating the corresponding
@@ -329,42 +316,33 @@ def system_init(N, max_mass, BHM, center, BHmomentum, ini_radius):
     '''
     bodies = []
     bodies.append(Node(BHM, position=center, momentum=BHmomentum))
-    #masses, positions, momenta = random_generate(N, max_mass, BHM, center, ini_radius)
-
-    #Parameters of the galaxy plane orientation 
-    beta=0      #Inclination
-    alpha=0     #Angle in the plain x,y
-    
-    masses, positions, momenta = spiral_galaxy(N, max_mass, BHM, center, ini_radius, alpha, beta)
+        
+    masses, positions, momenta = uniform_galaxy(N, max_mass, BHM, center, ini_radius)
+    #masses, positions, momenta = spiral_galaxy(N, max_mass, BHM, center, ini_radius, alpha, beta)
     for i in range(N-1):
-       bodies.append(Node(masses[i], positions[i], momenta[i]))
-    bodies=second_galaxy(bodies)
+       bodies.append(Node(masses[i], positions[i], momenta[i])) 
+    bodies=second_galaxy(bodies, ini_radius)
     return bodies
-    
+
 def evolve(bodies, n, center, ini_radius, img_step, image_folder='images/', video_name='my_video.mp4'):
     '''
     This function evolves the system in time using the Verlet algorithm and the Barnes-Hut octo-tree
     '''
-    # Limits for the axes in the plot
-    axis_limit = 1.5*ini_radius
-    lim_inf = [center[0]-axis_limit, center[1]-axis_limit, center[2]-axis_limit]
-    lim_sup = [center[0]+axis_limit, center[1]+axis_limit, center[2]+axis_limit]
     # Principal loop over time iterations.
     for i in range(n+1):
         # The octo-tree is recomputed at each iteration.
         root = None
         for body in bodies:
             body.reset_location()
-            root = add(body, root)
+            root = add(body, root) 
         # Evolution using the integration method
-        #verlet(bodies, root, theta, dt)
-        PEFRL(bodies, root, theta, dt)
+        verlet(bodies, root, theta, dt)
         # Write the image files
         if i%img_step==0:
             print("Writing image at time {0}".format(i))
-            plot_bodies(bodies, i//img_step, lim_inf, lim_sup, image_folder)
+            plot_bodies(bodies, i//img_step,image_folder)
 
-def plot_bodies(bodies, i, lim_inf, lim_sup, image_folder='images/'):
+def plot_bodies(bodies, i, image_folder='images/'):
     '''
     Writes an image file with the current position of the bodies
     '''
@@ -373,9 +351,9 @@ def plot_bodies(bodies, i, lim_inf, lim_sup, image_folder='images/'):
     #plt.rcParams['axes.labelcolor'] = 'dimgray'
     fig = plt.figure(figsize=(10,10), facecolor='black')
     ax = plt.gcf().add_subplot(111, projection='3d')
-    ax.set_xlim([lim_inf[0], lim_sup[0]])
-    ax.set_ylim([lim_inf[1], lim_sup[1]])
-    ax.set_zlim([lim_inf[2], lim_sup[2]])
+    ax.set_xlim([0,1])
+    ax.set_ylim([0,1])
+    ax.set_zlim([0,1])
     #ax.set_proj_type('ortho')
     ax.set_facecolor('black')
     ax.xaxis.pane.fill = False
@@ -384,6 +362,7 @@ def plot_bodies(bodies, i, lim_inf, lim_sup, image_folder='images/'):
     ax.xaxis.pane.set_edgecolor('dimgray')
     ax.yaxis.pane.set_edgecolor('dimgray')
     ax.zaxis.pane.set_edgecolor('dimgray')
+    ax.view_init(90, -90)
     #ax.xaxis.label.set_color('dimgray')
     #ax.yaxis.label.set_color('dimgray')
     #ax.zaxis.label.set_color('dimgray')
@@ -393,11 +372,14 @@ def plot_bodies(bodies, i, lim_inf, lim_sup, image_folder='images/'):
     #ax.grid(False)
     for body in bodies:
         pos = body.position()
-        ax.scatter(pos[0], pos[1], pos[2], marker='.', color='lightcyan')
+        if body.m>100.:
+            ax.scatter(pos[0], pos[1], pos[2], marker='.', color='lightcyan')
+        else:
+            ax.scatter(pos[0], pos[1], pos[2], marker='.', color='red')
+    print(" ")
     plt.gcf().savefig(image_folder+'bodies3D_{0:06}.png'.format(i))
     plt.close()
 
-""" 
 def create_video(image_folder='images/', video_name='my_video.mp4'):
     '''
     Creates a .mp4 video using the stored files images
@@ -425,35 +407,4 @@ def create_avi_video(image_folder='images/', video_name = 'video.avi'):
     for image in images:
         video.write(cv2.imread(join(image_folder, image)))
     cv2.destroyAllWindows()
-    video.release()
-"""
-"""
-if __name__=="__main__":
-    '''
-    Example of a randomly generated N-body system to be evolved using
-    the Barnes-Hut Algorithm and the Verlet method
-    '''
-    N = 200
-    # Masses of the N bodies (randomly generated)
-    mass = 10.*random.random(N) # Solar masses
-    # Supermassive Central Black Hole data
-    BHM = 4.e6 # Solar masses
-    center = array([0.5, 0.5, 0.5]) # Location of the SBH
-    BHmomentum = array([0.,0.,0.]) # Momentum of the SBH
-    # Initial radius of the distribution
-    ini_radius = 10. #kpc
-    # Number of time-iterations executed by the program.
-    n = 10000 # Time steps
-    # Frequency at which .PNG images are written.
-    img_step = 200
-    # Folder to save the images
-    image_folder = 'images/'
-    # Name of the generated video
-    video_name = 'my_video.mp4'
-    
-    #masses, positions, momenta = random_generate(N, 10., BHM, center, ini_radius)
-    #bodies = system_init(N, masses, positions, momenta, BHM, center, BHmomentum, ini_radius)
-    bodies = system_init(N, max_mass, BHM, center, BHmomentum, ini_radius)
-    print('Total number of bodies: ', len(bodies))
-    evolve(bodies, n, center, ini_radius, img_step, image_folder, video_name)
-    create_video(image_folder, video_name)"""
+    video.release()    
